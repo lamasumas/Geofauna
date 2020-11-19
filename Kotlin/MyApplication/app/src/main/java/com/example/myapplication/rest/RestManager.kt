8efccb9b1d6f1b7d48d15458f713ac2ebb0647dd
els <navesllanes@gmail.com>
2020-11-19 21:37:21 +0100

    Code documetned

--- Kotlin/MyApplication/app/src/main/java/com/example/myapplication/rest/RestManager.kt
+++ Kotlin/MyApplication/app/src/main/java/com/example/myapplication/rest/RestManager.kt
@@ -10,14 +10,23 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RestManager {
 
-
+    /**
+     * Observable for the nearest location
+     * @param lat, latitude
+     * @param lon, longitude
+     * @return obsercable of the list of nearest location
+     */
     fun getGeoNameLocation(lat: String, lon: String): Observable<List<GeoName>?>? {
-
-
         return Observable.just(getGeoNameLocationCall(lat, lon)).observeOn(Schedulers.io())
             .map { it.execute().body()?.geonames }.observeOn(AndroidSchedulers.mainThread())
     }
 
+    /**
+     * Rest call to the geonames api in order to get the nearest location
+     * @param lat, latitude
+     * @param lon, longitude
+     * @return rest call to the api
+     */
     private fun getGeoNameLocationCall(lat: String, lon: String): Call<GeoNameResponse> {
         val retroFit = Retrofit.Builder().baseUrl("http://api.geonames.org")
             .addConverterFactory(GsonConverterFactory.create()).build()
